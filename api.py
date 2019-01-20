eb79590eed36354d85c4efb42a1da99ac83e3073
Fabian Tamp <fabian.tamp@gmail.com>
2019-01-20 18:14:37 +0100

    Use ISO8601 timestamp formatting, add TODO to ensure that timezones are propagating properly. Ideally everything would be in UTC.

--- api.py
+++ api.py
@@ -22,5 +22,9 @@ def format_msg(msg):
     return {
         'content': msg.content,
         'username': msg.username,
-        'when_created': msg.when_created,
+        # It's not clear _why_ the default setup is returning "Sun, 20 Jan 2019 18:09:45 GMT", but
+        # using ISO8601 is going to make it a lot simpler when interpreting timestamps in JS.
+        # TODO: these don't have a timezone right now, which probably means that they'll do weird
+        #    things. Fix this.
+        'when_created': msg.when_created.isoformat(),
     }
