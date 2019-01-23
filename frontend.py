6f3b4484e43bac9b5d3d9dcf4baa2423dcefbfb3
Fabian Tamp <fabian.tamp@gmail.com>
2019-01-23 20:57:21 +0100

    Just give people random names, rather than do something complicated like build a name input.

--- frontend.py
+++ frontend.py
@@ -1,12 +1,55 @@
-from flask import Blueprint, render_template, request
+import random
 
-from models import db, Message
+from flask import Blueprint, render_template
 
 blueprint = Blueprint('frontend', __name__)
+
+# I grabbed this list from https://nameberry.com/unisex-names. If I get to
+# building a name-input dialog than we'll switch, but I kinda like just randomly
+# giving people a name.
+GENDER_NEUTRAL_NAMES = [
+    "Alexis",
+    "Amari",
+    "Angel",
+    "Ariel",
+    "Avery",
+    "Blake",
+    "Charlie",
+    "Dakota",
+    "Dallas",
+    "Eden",
+    "Elliot",
+    "Elliott",
+    "Emerson",
+    "Emery",
+    "Finley",
+    "Harley",
+    "Hayden",
+    "Jordan",
+    "Karter",
+    "Kendall",
+    "London",
+    "Marley",
+    "Morgan",
+    "Parker",
+    "Payton",
+    "Peyton",
+    "Phoenix",
+    "Quinn",
+    "Reese",
+    "Remington",
+    "Riley",
+    "River",
+    "Rowan",
+    "Rylan",
+    "Sawyer",
+    "Taylor",
+    "Zion",
+]
 
 
 @blueprint.route('/', methods=['GET'])
 def get_index():
-    # TODO: pagination
-    msgs = db.session.query(Message).all()
-    return render_template('index.html', messages=msgs)
+    random_username = random.choice(GENDER_NEUTRAL_NAMES)
+
+    return render_template('index.html', random_username=random_username)
