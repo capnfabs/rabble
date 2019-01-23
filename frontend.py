8d162a6dca296decf5a1bbb0daa12197a7b1d4ba
Fabian Tamp <fabian.tamp@gmail.com>
2019-01-23 20:09:06 +0100

    Add a real nasty version of the legit UI.

--- frontend.py
+++ frontend.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from models import db, Message
 
 blueprint = Blueprint('frontend', __name__)
 
+
 @blueprint.route('/', methods=['GET'])
 def get_index():
-    return render_template('index.html')
+    # TODO: pagination
+    msgs = db.session.query(Message).all()
+    return render_template('index.html', messages=msgs)
